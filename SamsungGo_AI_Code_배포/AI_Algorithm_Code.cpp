b4354997619bf11c578de8bf957fae1267dad237
= <=SangJoKim90@gmail.com>
2017-11-01 23:29:42 +0900

    arrange

--- SamsungGo_AI_Code_배포/AI_Algorithm_Code.cpp
+++ SamsungGo_AI_Code_배포/AI_Algorithm_Code.cpp
@@ -55,7 +55,6 @@ char info[] = {"TeamName:aura,Department:IoT사업화팀]"};
 #pragma region 변수초기화
 
 #define mp make_pair
-
 #define boardSize 19
 #define numOfDir 8
 #define numOfContinuousDir 4
@@ -69,24 +68,49 @@ enum progressDir { YX = 0, YMinusX, YY , XX };
 //돌의 타입 
 enum DolType {EMPTY = 0, ME, ENERMY, BLOCKING};
 
-//정답출력을 위한 정답 좌표.
-int ansX[2], ansY[2];
-
 
 //index가 방향. 0 = 아래, 1 = 오른쪽, 2 = 위, 3 = 왼쪽, 4 = 오른아래, 5 = 오른위, 6 = 왼아래, 7 = 왼위
 int dx[] = { 0, 1, 0, -1, 1, 1, -1, -1 };
 int dy[] = { 1, 0, -1, 0, 1, -1, 1, -1 };
+
+
+//정답출력을 위한 정답 좌표.
+int ansX[2], ansY[2];
+vector<int> myBoard(91, 0);
+
+//DFS를 통해 먼저 탐색할 만한 것들을 순서대로 나열해봄
+vector<pair<int, pair<int, int> > > myReference;
+vector<pair<int, pair<int, int> > > enermyReference;
+
+
+//방향 탐색을 위한 graph
+vector<vector<int> > dirAdj;
+vector<vector<int> > dirContinueAdj;
+
+
+//지금 판의 숫자를 계산하여 그 단계를 나눈다. 대략 돌의 개수가 30개정도 남았을 때에는 DP를 사용해 승리를 확정지을 수 있다.
+//하지만 전체판의 크기가 361개이므로 후반전으로 가기전에 초중반을 잘 계산해야한다.
+int totalMEandENERMY = 0;
+
+
+
 
 #pragma endregion
 
 
 #pragma region myBoard utility
 
+//(y, x)가 board 안이면 true, 밖이면 false
+bool isIn(int y, int x) {
+	return y >= 0 && x >= 0 && y < boardSize && x < boardSize;
+}
+
+
 
 //바둘돌을 두는 것 
 //(y, x)
 //who -> 1이면 나의 돌, 2이면 상대방의 돌
-void setYX(vector<int>& myBoard, int y, int x, int who) {
+void setYX(int y, int x, int who) {
 	int position = y * boardSize + x;
 	//cout << position << " "<< (position >> 4);
 	myBoard[position >> 4] |= (who << ((position % 16) * 2));
@@ -94,19 +118,25 @@ void setYX(vector<int>& myBoard, int y, int x, int who) {
 
 
 //(y, x)자리에 바둑돌을 없애는 것
-void unSetYX(vector<int>& myBoard, int y, int x, int who) {
+void unSetYX(int y, int x, int who) {
 	int position = y * boardSize + x;
 	myBoard[position >> 4] &= ~(3 << ((position % 16) * 2));
 }
 
 //(y, x)자리에 아무 것도 없는지 확인
-bool isEmpty(vector<int>& myBoard, int y, int x) {
+bool isEmpty(int y, int x) {
 	int position = y * boardSize + x;
 	return ((myBoard[position >> 4] >> (position % 16) * 2) & 3) == 0;
 }
 
+//(y, x)자리에 who가 있는지 확인
+bool isWho(int y, int x, int who) {
+	int position = y * boardSize + x;
+	return ((myBoard[position >> 4] >> (position % 16) * 2) & 3) == who;
+}
+
 //비트마스크가 잘되었는지 테스트용 출력함수
-void printMyBoard(vector<int>& myBoard){
+void printMyBoard(){
 
 	for (int y = 0; y < boardSize; y++){
 		for (int x = 0; x < boardSize; x++){
@@ -121,33 +151,26 @@ void printMyBoard(vector<int>& myBoard){
 
 //board[][]의 상태를 bitmask로 줄이는 것. myturn이 올 때마다 실행해서 계산의 초기화
 //int 91개면 board의 상태를 다 표현가능하다
-void myBoardInit(vector<int>& myBoard) {
+void myBoardInitWhenMyTurnIsCalled() {
 
 	for (int y = 0; y < boardSize; y++) {
 		for (int x = 0; x < boardSize; x++) {
 			//0 비어있다, 1 나의 돌, 2 상대방의 돌, 3 블러킹
-			setYX(myBoard, y, x, showBoard(x, y));
+			setYX(y, x, showBoard(x, y));
 		}
 	}
 }
 
-#pragma endregion
 
-
-#pragma region 재귀를 효율적으로 도와주는 애들
-
-//방향 탐색을 위한 graph
-vector<vector<int> > dirAdj;
-vector<vector<int> > dirContinueAdj;
 
 //방향 탐색을 위한 graph 초기화
-void dirAdjInit(){
+void dirAdjInit() {
 	//index가 방향. 0 = 아래, 1 = 오른쪽, 2 = 위, 3 = 왼쪽, 4 = 오른아래, 5 = 오른위, 6 = 왼아래, 7 = 왼위
 	// (0과 2) / (1과 3) / (4와 7) / (5와 6)
 	dirAdj = vector<vector<int> >(numOfDir);
 
 	//자기자신 추가
-	for (int dir = 0; dir < numOfDir; dir++){
+	for (int dir = 0; dir < numOfDir; dir++) {
 		dirAdj[dir].push_back(dir);
 	}
 
@@ -180,11 +203,54 @@ void dirAdjInit(){
 	dirContinueAdj[YMinusX].push_back(RIGHTDOWN);
 }
 
+#pragma endregion
 
-//(y, x)가 board 안이면 true, 밖이면 false
-bool isIn(int y, int x) {
-	return y >= 0 && x >= 0 && y < boardSize && x < boardSize;
+
+#pragma region 누가 이겼는지 판단
+
+int DFSForIsWin(bool (&isVisited)[boardSize][boardSize][numOfContinuousDir] ,int who, int y, int x, int dir) {
+
+	isVisited[y][x][dir] = true;
+
+	int ret = 1;
+
+	for (int i = 0; i < dirContinueAdj[dir].size(); i++) {
+		int nextY = y + dy[dirContinueAdj[dir][i]];
+		int nextX = x + dx[dirContinueAdj[dir][i]];
+		if(isIn(nextY, nextX) && isWho(nextY, nextX, who))
+			ret += DFSForIsWin(isVisited, who, nextY, nextX, dir);
+	}
+	return ret;
+
 }
+
+//기저사례 지금 board의 상태가 who가 이기면 true를, 그렇지 않으면 false를
+bool isWin(int who) {
+
+	bool isVisited[boardSize][boardSize][numOfContinuousDir];
+	
+	memset(isVisited, false, sizeof(isVisited));
+
+	int ret = 0;
+
+	for (int y = 0; y < boardSize; y++) {
+		for (int x = 0; x < boardSize; x++) {
+			for (int dir = 0; dir < numOfContinuousDir; dir++) {
+				ret = max(ret, DFSForIsWin(isVisited, who, y, x, dir));
+			}
+		}
+	}
+
+	//6보다 커지면 어떻게 될지 생각해봐야해
+	return ret == 6;
+}
+
+
+#pragma endregion
+
+
+#pragma region 탐색 순서 조장을 위한 Reference를 만드는 Logic
+
 
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -192,7 +258,7 @@ bool isIn(int y, int x) {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //내돌 혹은 적 입장에서 참고테이블을 만든다.  search를 빠르게 도와주는 역할임!!   연속된 돌의 숫자의 개수를 기록, dfs를 사용
-int DFSCalcReferenceTable(vector<vector<int> >& ReferenceBoard, vector<int>& myBoard, bool (&isVisited)[boardSize][boardSize][4], int hereY, int hereX, int hereDir,int who ,vector<pair<int, int> >& record){
+int DFSCalcReferenceTable(vector<vector<int> >& ReferenceBoard, bool(&isVisited)[boardSize][boardSize][4], int hereY, int hereX, int hereDir, int who, vector<pair<int, int> >& record) {
 
 	isVisited[hereY][hereX][hereDir] = true;
 	record.push_back(mp(hereY, hereX));
@@ -200,40 +266,39 @@ int DFSCalcReferenceTable(vector<vector<int> >& ReferenceBoard, vector<int>& myB
 	int ret = 1;
 	bool check = false;
 	//hereDir = 0이면 세로 방향, hereDir = 1이면 가로방향, hereDir = 2이면 y = x방향, hereDir = 3이면 y = -x방향
-	for (int i = 0; i < dirContinueAdj[hereDir].size(); i++){
+	for (int i = 0; i < dirContinueAdj[hereDir].size(); i++) {
 		int nextY = hereY + dy[dirContinueAdj[hereDir][i]];
 		int nextX = hereX + dx[dirContinueAdj[hereDir][i]];
 		//다음 탐색의 범위가 보드의 범위 안쪽이고, 거기가 내 것이여야만 탐색을 진행한다.
-		if (isIn(nextY, nextX) && !isVisited[nextY][nextX][hereDir] && showBoard(nextX, nextY) == who){
-			ret += DFSCalcReferenceTable(ReferenceBoard, myBoard, isVisited, nextY, nextX, hereDir, who, record);
+		if (isIn(nextY, nextX) && !isVisited[nextY][nextX][hereDir] && isWho(nextY, nextX, who)) {
+			ret += DFSCalcReferenceTable(ReferenceBoard, isVisited, nextY, nextX, hereDir, who, record);
 		}
 
 		int nNextY = nextY + dy[dirContinueAdj[hereDir][i]];
 		int nNextX = nextX + dx[dirContinueAdj[hereDir][i]];
 		int enermy = (who == ME ? ENERMY : ME);
-		if (isIn(nNextY, nNextX) && !isVisited[nNextY][nNextX][hereDir] && showBoard(nNextX, nNextY) == who &&
-			(showBoard(nextX, nextY) != enermy && showBoard(nextX, nextY) != BLOCKING)){
-			ret += DFSCalcReferenceTable(ReferenceBoard, myBoard, isVisited, nNextY, nNextX, hereDir, who, record);
+		if (isIn(nNextY, nNextX) && !isVisited[nNextY][nNextX][hereDir] && isWho(nNextY, nNextX, who) &&
+			!isWho(nextY, nextX, enermy) && !isWho(nextY, nextX, BLOCKING)) {
+			ret += DFSCalcReferenceTable(ReferenceBoard, isVisited, nNextY, nNextX, hereDir, who, record);
 		}
 	}
-
 	return ret;
 }
 
 //우리편 입장에서 참고테이블을 만든다.  search를 빠르게 도와주는 역할임!!   연속된 돌의 숫자의 개수를 기록, dfs를 사용
 //돌의 가중치를 만들어 이 것부터 탐색하게 만든다.
-void calcMyReferenceTable(vector<vector<int> >& myReferenceBoard, vector<int>& myBoard){
+void calcMyReferenceTable(vector<vector<int> >& myReferenceBoard) {
 	bool isVisited[boardSize][boardSize][numOfContinuousDir];
 	memset(isVisited, false, sizeof(isVisited));
-	for (int y = 0; y < boardSize; y++){
-		for (int x = 0; x < boardSize; x++){
-			for (int dir = 0; dir < numOfContinuousDir; dir++){
+	for (int y = 0; y < boardSize; y++) {
+		for (int x = 0; x < boardSize; x++) {
+			for (int dir = 0; dir < numOfContinuousDir; dir++) {
 				//아직 방문하지 않았고 그 위치가 지금 비어있으면 들어간다.
-				if (!isVisited[y][x][dir] && isFree(x, y)){
+				if (!isVisited[y][x][dir] && isFree(x, y)) {
 					//연속된 갯수가 numOfContinue에, 그 위치가 record에 반환
 					vector<pair<int, int> > record;
-					int numOfContinue = DFSCalcReferenceTable(myReferenceBoard, myBoard, isVisited, y, x, dir, ME, record);
-					for (int i = 0; i < record.size(); i++){
+					int numOfContinue = DFSCalcReferenceTable(myReferenceBoard, isVisited, y, x, dir, ME, record);
+					for (int i = 0; i < record.size(); i++) {
 						myReferenceBoard[record[i].first][record[i].second] = numOfContinue;
 					}
 				}
@@ -244,18 +309,18 @@ void calcMyReferenceTable(vector<vector<int> >& myReferenceBoard, vector<int>& m
 
 //적 입장에서 참고 테이블을 만든다. search를 빠르게 도와주는 역할! 연속된 돌의 숫자의 개수를 기록, dfs를 사용!
 //돌의 가중치를 만들어 이 것 부터 탐색하게 만든다.
-void calcEnermyReferenceTable(vector<vector<int> >& enermyReferenceBoard, vector<int>& myBoard){
+void calcEnermyReferenceTable(vector<vector<int> >& enermyReferenceBoard) {
 	bool isVisited[boardSize][boardSize][numOfContinuousDir];
 	memset(isVisited, false, sizeof(isVisited));
-	for (int y = 0; y < boardSize; y++){
-		for (int x = 0; x < boardSize; x++){
-			for (int dir = 0; dir < numOfContinuousDir; dir++){
+	for (int y = 0; y < boardSize; y++) {
+		for (int x = 0; x < boardSize; x++) {
+			for (int dir = 0; dir < numOfContinuousDir; dir++) {
 				//아직 방문하지 않았고 그 위치가 지금 비어있으면 들어간다.
-				if (!isVisited[y][x][dir] && isFree(x, y)){
+				if (!isVisited[y][x][dir] && isFree(x, y)) {
 					//연속된 갯수가 numOfContinue에, 그 위치가 record에 반환
 					vector<pair<int, int> > record;
-					int numOfContinue = DFSCalcReferenceTable(enermyReferenceBoard, myBoard, isVisited, y, x, dir, ENERMY, record);
-					for (int i = 0; i < record.size(); i++){
+					int numOfContinue = DFSCalcReferenceTable(enermyReferenceBoard, isVisited, y, x, dir, ENERMY, record);
+					for (int i = 0; i < record.size(); i++) {
 						enermyReferenceBoard[record[i].first][record[i].second] = numOfContinue;
 					}
 				}
@@ -264,28 +329,56 @@ void calcEnermyReferenceTable(vector<vector<int> >& enermyReferenceBoard, vector
 	}
 }
 
+
+void makeReference() {
+
+	//탐색을 위한 참고테이블. 연속으로 몇개가 놓여져 있는지를 그 위치에 기록 -> 이 것 우선 순위로 탐색을 위함
+	//연속 되었으나 막혀 있으면 -1
+	vector<vector<int> > myReferenceTable(boardSize, vector<int>(boardSize, -1));
+	calcMyReferenceTable(myReferenceTable);
+
+	//탐색을 위한 참고테이블. 적의 개수. 방어를 위함
+	vector<vector<int> > enermyReferenceTable(boardSize, vector<int>(boardSize, -1));
+	calcEnermyReferenceTable(enermyReferenceTable);
+
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	// ReferenceTable 만든 것들 test해야함!
+	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+	//myReferenceTable 과 enermyReferenceTable에서 만든 가중치를 sorting하여 search에 참조하게 만든다.
+	//-1인 것을 굳이 넣을 필요는 있을까? 생각해보아야 할 문제
+	for (int y = 0; y < boardSize; y++) {
+		for (int x = 0; x < boardSize; x++) {
+			if (myReferenceTable[y][x] != -1) {
+				myReference.push_back(mp(myReferenceTable[y][x], mp(y, x)));
+			}
+			if (enermyReferenceTable[y][x] != -1) {
+				enermyReference.push_back(mp(enermyReferenceTable[y][x], mp(y, x)));
+			}
+		}
+	}
+	sort(myReference.begin(), myReference.end()); sort(enermyReference.begin(), enermyReference.end());
+
+}
+
+
+
 #pragma endregion
 
 
-//지금 판의 숫자를 계산하여 그 단계를 나눈다. 대략 돌의 개수가 30개정도 남았을 때에는 DP를 사용해 승리를 확정지을 수 있다.
-//하지만 전체판의 크기가 361개이므로 후반전으로 가기전에 초중반을 잘 계산해야한다.
-int totalMEandENERMY = 0;
-
-
-//DFS를 통해 먼저 탐색할 만한 것들을 순서대로 나열해봄
-vector<pair<int, pair<int, int> > > myReference;
-vector<pair<int, pair<int, int> > > enermyReference;
-
+#pragma region 본격 재귀 -> 점점 깊어지는 DFS로 구현. 필수적인 정보들을 위에 선언.
 
 
 //본격 재귀 함수. 우선 백트래킹으로, 향후 가지치기, 부분 DP 등 확장
 //who = 누구의 차례인지 who = 1 나의 차례, who = 2 적의 차례
 //depth = 재귀의 깊이 , 홀수이면 나의 차례, 짝수이면 적의 차례
 //2개의 수를 놓음
-void search(vector<int>& myBoard, int who, int depth) {
+void search(int who, int depth) {
 
 	cout << depth << endl;
-	int enermy = (who == ME ? ENERMY : ME);
+
+	int opposite = (who == ME ? ENERMY : ME);
 
 	//앞으로 10수까지만 생각한다. 향후 조정. 시간 및 탐색의 범위를 설정하면 더 많은 수를 계산할 수 있으므로
 	if (depth >= 3) return;
@@ -295,16 +388,19 @@ void search(vector<int>& myBoard, int who, int depth) {
 	for (int y = 0; y < boardSize; y++){
 		for (int x = 0; x < boardSize; x++){
 			//비어있으면
-			if (isEmpty(myBoard, y, x)){
+			if (isEmpty(y, x)){
 
-				setYX(myBoard, y, x, who);
-				search(myBoard, enermy, depth + 1);
-				unSetYX(myBoard, y, x, who);
+				setYX(y, x, who);
+				search(opposite, depth + 1);
+				unSetYX(y, x, who);
 
 			}
 		}
 	}
 }
+
+
+#pragma endregion
 
 
 
@@ -313,37 +409,9 @@ void search(vector<int>& myBoard, int who, int depth) {
 //시간에 대한 부분도 추후 넣어야할듯.
 void AURAStart(){
 
-	vector<int> myBoard(91, 0);
-	myBoardInit(myBoard);
-
-
-	//탐색을 위한 참고테이블. 연속으로 몇개가 놓여져 있는지를 그 위치에 기록 -> 이 것 우선 순위로 탐색을 위함
-	//연속 되었으나 막혀 있으면 -1
-	vector<vector<int> > myReferenceTable(boardSize, vector<int>(boardSize, -1));
-	calcMyReferenceTable(myReferenceTable, myBoard);
+	myBoardInitWhenMyTurnIsCalled();
+	makeReference();
 	
-	//탐색을 위한 참고테이블. 적의 개수. 방어를 위함
-	vector<vector<int> > enermyReferenceTable(boardSize, vector<int>(boardSize, -1));
-	calcEnermyReferenceTable(enermyReferenceTable, myBoard);
-
-	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	// ReferenceTable 만든 것들 test해야함!
-	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	
-
-	//myReferenceTable 과 enermyReferenceTable에서 만든 가중치를 sorting하여 search에 참조하게 만든다.
-	//-1인 것을 굳이 넣을 필요는 있을까? 생각해보아야 할 문제
-	for (int y = 0; y < boardSize; y++){
-		for (int x = 0; x < boardSize; x++){
-			if (myReferenceTable[y][x] != -1){
-				myReference.push_back(mp(myReferenceTable[y][x], mp(y, x)));
-			}
-			if (enermyReferenceTable[y][x] != -1){
-				enermyReference.push_back(mp(enermyReferenceTable[y][x], mp(y, x)));
-			}
-		}
-	}
-	sort(myReference.begin(), myReference.end()); sort(enermyReference.begin(), enermyReference.end());
 
 
 
@@ -372,21 +440,16 @@ void myturn(int cnt) {
 		return;
 	}
 
-	cout << dirAdj.size() << endl;
 	AURAStart();
 
+	for (int y = 0; y < 6; y++) {
+		setYX(y, 0, ME);
+	}
 
-	/*srand((unsigned)time(NULL));
+	cout << isWin(ME) << endl;
 
-	for (int i = 0; i < cnt; i++) {
-	do {
-	x[i] = rand() % width;
-	y[i] = rand() % height;
-	if (terminateAI) return;
-	} while (!isFree(x[i], y[i]));
 
-	if (x[1] == x[0] && y[1] == y[0]) i--;
-	}*/
+
 
 
 	// 이 부분에서 자신이 놓을 돌을 출력하십시오.
